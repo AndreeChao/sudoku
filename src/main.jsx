c9b8e1fa33adfb6a7dfb6dd7fa236b35a03d8ae4
AndreeChao <andreechao@gmail.com>
2026-06-28 01:26:49 +0800

    debug: add error boundary to catch blank page crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
-import { StrictMode } from 'react'
+import { StrictMode, Component } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app/App.jsx'
 
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null }
+  }
+  static getDerivedStateFromError(error) {
+    return { error }
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ padding: 24, fontFamily: 'monospace', fontSize: 14, color: '#c00' }}>
+          <strong>Error:</strong><br />
+          {this.state.error.message}<br /><br />
+          {this.state.error.stack}
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 )
